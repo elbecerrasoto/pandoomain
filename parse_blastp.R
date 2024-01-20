80a6e12c52282a7d1cb86b7a874ddf4ea2d38181
Emanuel Becerra Soto <elbecerrasoto@gmail.com>
2024-01-20 07:10:38 -0700

    wip

--- parse_blastp.R
+++ parse_blastp.R
@@ -1,12 +1,17 @@
 library(tidyverse)
 
-BLASTP <- "blastp.tsv"
+BLASTP <- "/home/ebecerra/2-projects/viuva/results/genomes/GCA_000743215.1/blastp.tsv"
 HEADER <- c("QueryID", "SubjectID", "PercentageIdentity", "QueryCoverage", "SubjectCoverage", "EValue")
+
+IDENTITY <- 30
+QCOVERAGE <- 60
+SCOVERAGE <- 60
+EVAL <- 0.05
 
 blastp <- read_tsv(BLASTP, col_names = HEADER, comment = "#",)
 
 blastp |> 
-  filter(PercentageIdentity >= 30, 
-         QueryCoverage >= 60,
-         SubjectCoverage >= 60,
-         EValue <= 0.05)
+  filter(PercentageIdentity >= IDENTITY, 
+         QueryCoverage >= QCOVERAGE,
+         SubjectCoverage >= SCOVERAGE,
+         EValue <= EVAL)
