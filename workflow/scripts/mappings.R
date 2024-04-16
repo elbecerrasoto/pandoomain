b5246ad56504bc9eecff10e009c75a1f341e76df
Emanuel Becerra Soto <elbecerrasoto@gmail.com>
2024-04-16 14:15:00 -0700

    feat: mappings script

--- workflow/scripts/mappings.R
+++ workflow/scripts/mappings.R
@@ -1,9 +1,15 @@
+#!/usr/bin/Rscript
+
 library(yaml)
+library(magrittr)
 library(tidyverse)
+
 
 args <- commandArgs(trailingOnly = T)
 
+
 # Globals ----
+
 
 CONFIG <- args[1]
 ISCAN <- args[2]
@@ -13,21 +19,9 @@ CONFIG <- "tests/config.yaml"
 ISCAN <- "tests/results/iscan.tsv"
 BLASTS <- "tests/results/blasts.tsv"
 
-# Read ----
-
-config <- read_yaml(CONFIG)
-
-filters <- config$filtering_domains
-aliases <- config$query_aliases
-
-pids2filter <- names(filters)
-pids2alias <- names(aliases)
-
-iscan <- read_tsv(ISCAN, na = c("-", "NA", ""))
-
-blasts <- read_tsv(BLASTS)
 
 # Helpers ----
+
 
 is_tbl_NA_free <- function(tbl) {
   tbl |>
@@ -35,68 +29,91 @@ is_tbl_NA_free <- function(tbl) {
     all()
 }
 
+stop_on_NA <- function(tbl) stopifnot(is_tbl_NA_free(tbl))
 
-switch_vectorized <- function(v_chr, switch_list) {
-  f <- function(v_chr) {
-    do.call(
-      switch,
-      c(v_chr, switch_list, NA)
-    )
-  }
-  map_chr(v_chr, f)
+
+# Read Data ----
+
+
+config <- read_yaml(CONFIG)
+
+filtersL <- config$filtering_domains
+aliasesL <- config$query_aliases
+
+iscan <- read_tsv(ISCAN, na = c("-", "NA", ""))
+
+blasts <- read_tsv(BLASTS) |>
+  rename(
+    query = qseqid,
+    pid = sseqid
+  )
+
+
+# Mappings ----
+
+
+# 1->M one to many
+# M->M many to many
+
+# The output table contains the following relations
+# aliases 1->1 queries 1->M pids M->M domains
+# aliases 1->1 queries 1->M pids M->M genomes
+
+
+# setup aliases 1->1 queries
+queries <- unique(blasts$query)
+
+assign_alias <- function(query, alias_dict) {
+  alias <- alias_dict[[query]]
+  present <- !is.null(alias)
+  ifelse(present, alias, query)
 }
 
-length(paste(letters))
-str_flatten(letters, collapse = ";")
+aliases <- queries |>
+  map_chr(assign_alias, alias_dict = aliasesL)
 
-# Main ----
+# aliases 1->1 queries
+alias2query <- tibble(
+  q_alias = aliases,
+  query = queries
+) %T>%
+  stop_on_NA()
 
-# Mappings
-# queries 1->M pids M->M domains
-# queries 1->M pids M->M genomes
 
 # queries 1->M pids
-q2pids <- blasts |>
-  rename(query = qseqid) |>
+query2pids <- blasts |>
   group_by(query) |>
-  reframe(pid = unique(sseqid))
-
-stopifnot(is_tbl_NA_free(q2pids))
-
-
-if (F) {
-  # Add the aliases
-  q2pids <- q2pids |>
-    mutate(q_alias = switch_vectorized(
-      query,
-      map(QUERIES_ALIASES, \(x) x)
-    )) |>
-    relocate(q_alias)
+  reframe(pid = unique(pid)) %T>%
+  stop_on_NA()
 
 
-  q2pids
-
-  blasts
-  blasts |>
-    group_by(sseqid) |>
-    summarise(genomes = str_flatten(unique(genome), collapse = ";"))
-
+# pids M->M domains
+pid2domains <- iscan |>
+  group_by(pid) |>
+  reframe(domain = unique(interpro[!is.na(interpro)])) %T>%
+  stop_on_NA()
 
 
+# pids M->M genomes
+pid2genomes <- blasts |>
+  group_by(pid) |>
+  reframe(genome = unique(genome)) %T>%
+  stop_on_NA()
 
 
-  # Map each pid to domains (is a 1-to-many mapping)
-  pid2doms <- iscan |>
-    group_by(protein) |>
-    reframe(domain = unique(interpro[!is.na(interpro)])) |>
-    rename(pid = protein)
-
-  stopifnot(is_tbl_NA_free(pid2doms))
+# Join and Output ----
 
 
-  # Join both tables
-  q2pids2domains <- left_join(q2pids, pid2doms)
+mappings <- alias2query |>
+  left_join(query2pids, join_by(query)) %T>%
+  stop_on_NA() |>
+  left_join(pid2domains, join_by(pid),
+    relationship = "many-to-many"
+  ) |>
+  left_join(pid2genomes, join_by(pid),
+    relationship = "many-to-many"
+  )
 
-  q2pids2domains |>
-    write_tsv(OUT)
-}
+mappings |>
+  format_tsv() |>
+  writeLines(stdout(), sep = "")
