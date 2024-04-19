f27f50e571b4ebd2cd277654861285860def2ae9
Emanuel Becerra Soto <elbecerrasoto@gmail.com>
2024-04-18 18:13:39 -0700

    utils: generate_cache ready for the action

--- utils/generate_cache.py
+++ utils/generate_cache.py
@@ -5,32 +5,6 @@ from typing import Union
 from argparse import ArgumentParser
 
 
-INCLUDE_DEF = ["genome", "protein", "gff3"]
-CWD = Path(os.getcwd())
-
-parser = argparse.ArgumentParser(
-    description=DESCRIPTION, formatter_class=argparse.RawDescriptionHelpFormatter
-)
-parser.add_argument(
-    "--C-grep", type=int, help="grep context argument -C, when reducing the gff."
-)
-parser.add_argument("--config", help="config.yaml to pass to snakemake.")
-parser.add_argument(
-    "--pids", nargs="+", help="protein IDs used to reduce, separated by '|'."
-)
-parser.add_argument(
-    "--faas", nargs="+", help="faa files, same order as corresponding gff."
-)
-parser.add_argument(
-    "--gffs", nargs="+", help="gff files, same order as corresponding faa."
-)
-
-args = parser.parse_args()
-
-C_GREP = args.C_grep
-CONFIG = args.config
-
-
 def run(
     cmd: Union[str, list], dry: bool = False, shell: bool = True, verbose: bool = True
 ):
@@ -44,12 +18,62 @@ def run(
         sp.run(cmd, shell=shell, check=True)
 
 
-def genomes2ext(ext: str) -> list:
-    return [Path(f"{GENOMES_DIR}/{genome}/{genome}{ext}") for genome in GENOMES]
-
-
 def each2str(l: list) -> list:
     return [str(i) for i in l]
+
+
+parser = ArgumentParser(
+    description="Generates test data for the hoox snakemake pipeline."
+)
+
+parser.add_argument(
+    "--C-grep",
+    type=int,
+    help="grep context argument -C, used when reducing the gffs.",
+    required=True,
+)
+parser.add_argument("--config", help="config.yaml to pass to snakemake.", required=True)
+parser.add_argument(
+    "--cache-dir",
+    type=Path,
+    help="directory to generate the reduced files.",
+    required=True,
+)
+parser.add_argument(
+    "--genomes-dir",
+    type=Path,
+    help="directory where snakemake generates gffs and faas files.",
+    required=True,
+)
+
+parser.add_argument(
+    "--pids",
+    nargs="+",
+    help="protein IDs, used to reduce gffs, and faas, e.g. WP_072173795.1",
+    required=True,
+)
+parser.add_argument(
+    "--genomes",
+    nargs="+",
+    help="genome list, assembly accessions, e.g. GCF_001286845.1",
+    required=True,
+)
+args = parser.parse_args()
+
+C_GREP = args.C_grep
+
+CONFIG = Path(args.config)
+assert CONFIG.exists(), f"{CONFIG} does not exist."
+
+CACHE_DIR = Path(args.cache_dir)
+GENOMES_DIR = Path(args.genomes_dir)
+
+PIDs = "|".join(args.pids)
+GENOMES = args.genomes
+
+
+def genomes2ext(ext: str) -> list:
+    return [Path(f"{GENOMES_DIR}/{genome}/{genome}{ext}") for genome in GENOMES]
 
 
 GFFs = genomes2ext(".gff")
@@ -72,7 +96,7 @@ def reduce_genomes():
     for idx, genome in enumerate(GENOMES):
         run(f"fasta_extract '{PIDs}' < {FAAs[idx]} > {CACHE_DIR}/{FAAs[idx].name}")
         run(
-            f"grep --perl-regexp -C {N} '{PIDs}' {GFFs[idx]} > {CACHE_DIR}/{GFFs[idx].name}"
+            f"grep --perl-regexp -C {C_GREP} '{PIDs}' {GFFs[idx]} > {CACHE_DIR}/{GFFs[idx].name}"
         )
 
 
