26c0e9ed8ec2d29ab26ede1908834f70233cd6b7
Emanuel Becerra Soto <elbecerrasoto@gmail.com>
2024-05-24 15:08:32 -0700

    wip: desempates extracted

--- edit.py
+++ edit.py
@@ -1,20 +1,31 @@
 #!/usr/bin/env ipython
 
-import pandas as pd
 import re
 
-GENOME_REGEX = r"^GC[AF]_\d+\.\d$"
+import pandas as pd
+
+IN = "genomes_messy.txt"
+
+GENOME_REGEX = r"^GC[AF]_\d+\.\d+$"
+REFSEQ_REGEX = r"^GCF_"
+ID_REGEX = r"^GC[AF]_(\d+)\.\d+$"
+VERSION_REGEX = r"^GC[AF]_\d+\.(\d+)$"
 
 
 def remove_comments(x: str) -> str:
-    import re
     return re.sub(r"#.*$", "", x).strip()
 
-df = pd.read_table("genomes_messy.txt", names = ("genome",), sep = "\t")
+
+df = pd.read_table(IN, names=("genome",), sep="\t")
 df.genome = df.genome.apply(remove_comments)
 
 genome_matches = [bool(re.match(GENOME_REGEX, g)) for g in df.genome]
 
 df = df.loc[genome_matches, :]
+
+
+df["refseq"] = df.genome.apply(lambda x: bool(re.search(REFSEQ_REGEX, x)))
+df["id"] = df.genome.apply(lambda x: int(re.search(ID_REGEX, x).group(1)))
+df["version"] = df.genome.apply(lambda x: int(re.search(VERSION_REGEX, x).group(1)))
 
 print(df)
