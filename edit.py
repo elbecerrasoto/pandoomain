42c0887cd16e497f68f037656c1335823b2758ea
Emanuel Becerra Soto <elbecerrasoto@gmail.com>
2024-05-28 14:36:18 -0600

    external pandas deduplication done

--- edit.py
+++ edit.py
@@ -5,6 +5,8 @@ import re
 import pandas as pd
 
 IN = "genomes_messy.txt"
+OUT = "genomes.tsv"
+ONLY_REFSEQ = False
 
 GENOME_REGEX = r"^GC[AF]_\d+\.\d+$"
 REFSEQ_REGEX = r"^GCF_"
@@ -28,4 +30,12 @@ df["refseq"] = df.genome.apply(lambda x: bool(re.search(REFSEQ_REGEX, x)))
 df["id"] = df.genome.apply(lambda x: int(re.search(ID_REGEX, x).group(1)))
 df["version"] = df.genome.apply(lambda x: int(re.search(VERSION_REGEX, x).group(1)))
 
-print(df)
+df = df.drop_duplicates()
+df = df.sort_values(["id", "version", "refseq"], ascending=False)
+
+
+if ONLY_REFSEQ:
+    df = df[df.refseq]
+
+df = df.groupby("id").first()
+df.to_csv(OUT, sep="\t")
