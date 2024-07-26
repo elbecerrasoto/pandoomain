8fad07985943e48cf046f61c52c91f0b724f3b48
Emanuel Becerra Soto <elbecerrasoto@gmail.com>
2024-07-26 12:18:42 -0700

    take samples grouped by genus

---
+++ max_genus.R
@@ -0,0 +1,31 @@
+#!/usr/bin/env Rscript
+
+library(tidyverse)
+library(stringr)
+
+argv <- commandArgs(trailingOnly = TRUE)
+MAX <- as.integer(argv[1])
+IN <- argv[2]
+# MAX <- 64
+# IN <- "clean_bacillota.tsv"
+
+meta <- read_tsv(IN)
+
+sample_n_all <- function(x, n) {
+  N <- nrow(x)
+  if (n <= N) {
+    return(sample_n(x, n, replace = FALSE))
+  } else {
+    return(x)
+  }
+}
+
+meta <- meta |>
+  group_by(genus) |>
+  group_modify(~ sample_n_all(.x, MAX)) |>
+  ungroup() |>
+  relocate(genus, .after = org)
+
+meta |>
+  format_tsv() |>
+  writeLines(stdout(), sep = "")
