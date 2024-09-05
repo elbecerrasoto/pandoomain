d590d4db7477ac9d94a3e8e249eede66bf739f23
Emanuel Becerra Soto <elbecerrasoto@gmail.com>
2024-09-05 03:02:53 -0700

    non longer needed it scripts

--- create_links.sh
+++
@@ -1,4 +0,0 @@
-parallel 'mkdir -p results/genomes/{} && ln -s /run/media/ebecerra/lab_WIP/GenomeDownload/bacillota/{}/{}.faa results/genomes/{}' :::: bacillota.wflow.txt
-parallel 'mkdir -p results/genomes/{} && ln -s /run/media/ebecerra/lab_WIP/GenomeDownload/bacillota/{}/{}_cds.tsv results/genomes/{}' :::: bacillota.wflow.txt
-parallel 'mkdir -p results/genomes/{} && ln -s /run/media/ebecerra/lab_WIP/GenomeDownload/bacillota/{}/{}.gff results/genomes/{}' :::: bacillota.wflow.txt
-
