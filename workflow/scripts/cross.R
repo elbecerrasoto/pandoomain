540a0e081d149277bac9b746ea69cdd8e6060ad9
Emanuel Becerra Soto <elbecerrasoto@gmail.com>
2024-10-23 15:21:29 -0600

    get the table to cross

---
+++ workflow/scripts/cross.R
@@ -0,0 +1,31 @@
+#!/usr/bin/env Rscript
+library(tidyverse)
+
+TAXID_ALL <- "tests/results/.taxallnomy_lin_name.txt"
+TAXID_HITS <- "tests/results/genomes_metadata.tsv"
+
+TREE <- "tests/results/taxallnomy_lin_name.tsv"
+
+taxid_all <- read_tsv(TAXID_ALL, col_names = F)[[1]]
+taxid_hits <- read_tsv(TAXID_HITS)$tax_id
+
+# Test if is faster with unique or sort
+rows_on_read <- sort(which(taxid_all %in% taxid_hits))
+
+tree <- file(TREE, "r")
+
+i <- 1
+row <- 1
+while(0 != length(line <- readLines(tree, n = 1))) {
+  if (rows_on_read[[row]] == i) {
+    cat(line)
+    
+    if (row == length(rows_on_read)) break
+    
+    row <- row + 1
+  }
+  
+  i <- i + 1
+}
+
+close(tree)
