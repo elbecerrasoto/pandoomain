d85f53fee2f6a2dc625c0c6b8b1a86a5c0c51f06
Emanuel Becerra Soto <elbecerrasoto@gmail.com>
2024-10-24 05:10:28 -0600

    cross refactor into reading everything to RAM
    ---
    It is not fast,
    about 1 min.

--- workflow/scripts/cross.R
+++ workflow/scripts/cross.R
@@ -1,39 +1,49 @@
 #!/usr/bin/env Rscript
+
+# Join Manually
+# genomes_metadata.tsv & taxallnomy_lin_name.tsv
+# by taxid
+# why? to only use the relevant part of the table taxallnomy table.
+
 library(tidyverse)
 
 TAXID_ALL <- "tests/results/.taxallnomy_lin_name.txt"
-TAXID_HITS <- "tests/results/genomes_metadata.tsv"
+TAXID_GENOMES <- "tests/results/genomes_metadata.tsv"
 
 TREE <- "tests/results/taxallnomy_lin_name.tsv"
 
-taxid_all <- read_tsv(TAXID_ALL, col_names = F)[[1]]
-taxid_hits <- read_tsv(TAXID_HITS)$tax_id
+NAMES <- c(
+  "tax_id", "superkingdom", "Kin", "sbKin",
+  "spPhy", "phylum", "sbPhy", "inPhy", "spCla",
+  "class", "sbCla", "inCla", "Coh", "sbCoh",
+  "spOrd", "order", "sbOrd", "inOrd", "prOrd",
+  "spFam", "family", "sbFam", "Tri", "sbTri",
+  "genus", "sbGen", "Sec", "sbSec", "Ser",
+  "sbSer", "Sgr", "sbSgr", "species", "Fsp",
+  "sbSpe", "Var", "sbVar", "For", "Srg",
+  "Srt", "Str", "Iso"
+)
 
-# Test if is faster with unique or sort
-rows_on_read <- sort(which(taxid_all %in% taxid_hits))
 
+# styler: off
+genomes    <- read_tsv(TAXID_GENOMES) |>
+  select(genome, tax_id)
+taxallnomy <- read_tsv(TAXID_ALL, col_names = "tax_id")
 
-# subset the taxallnomy tablet
+taxid_all     <-     taxallnomy$tax_id
+taxid_genomes <- unique(genomes$tax_id)
+# styler: on
+
+rows <- sort(which(taxid_all %in% taxid_genomes))
+
+# subset the taxallnomy table
 # to only relevant entries
 
-tree <- file(TREE, "r")
+tree <- read_tsv(TREE, col_names = NAMES)
+relevant <- tree[rows, ]
+rm(tree)
 
-i <- 1
-row <- 1
-while(0 != length(line <- readLines(tree, n = 1))) {
-  if (rows_on_read[[row]] == i) {
-    # cat(line) slow writes
-    
-    if (row == length(rows_on_read)) break
-    
-    row <- row + 1
-  }
-  
-  i <- i + 1
-}
-
-close(tree)
-
-# Hold on RAM, do not write
-# WIP, do the join
-
+genomes |>
+  left_join(relevant, join_by(tax_id)) |>
+  format_tsv() |>
+  writeLines(stdout(), sep = "")
