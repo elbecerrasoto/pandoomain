70056260d7b7416229418447be745b3864546652
Emanuel Becerra Soto <elbecerrasoto@gmail.com>
2024-10-29 13:30:17 -0700

    feat: absence_prescence matrix script

---
+++ workflow/scripts/absence_presence.R
@@ -0,0 +1,65 @@
+#!/usr/bin/env Rscript
+
+suppressPackageStartupMessages({
+  library(tidyverse)
+})
+
+GENOMES <- "tests/results/genomes_metadata.tsv"
+PROTEINS <- "tests/results/hmmer.tsv"
+DOMAINS <- "tests/results/archs.tsv"
+
+RANKS <- "tests/results/genomes_ranks.tsv"
+RANKS_SEL <- c(
+  "genome", "tax_id",
+  "superkingdom",
+  "phylum", "class", "order",
+  "family", "genus", "species"
+)
+
+WRITE_TGPD <- FALSE
+
+ranks <- read_tsv(RANKS) |>
+  select(all_of(RANKS_SEL))
+genomes <- read_tsv(GENOMES) |>
+  select(genome, tax_id) |>
+  mutate(tax_id = as.integer(tax_id))
+proteins <- read_tsv(PROTEINS) |>
+  select(genome, pid)
+domains <- read_tsv(DOMAINS) |>
+  select(pid, domain)
+
+# Taxid 1-m Genomes m-m Proteins m-m Domains
+# 1-1 one-to-one
+# 1-m one-to-many
+# m-m many-to-many
+
+TGPD <- genomes |>
+  left_join(proteins, join_by(genome),
+    relationship = "many-to-many"
+  ) |>
+  left_join(domains, join_by(pid),
+    relationship = "many-to-many"
+  )
+
+if (WRITE_TGPD) {
+  write_tsv(TGPD, "TGPD.tsv")
+}
+
+absence_presence <- TGPD |>
+  select(-pid) |>
+  distinct() |>
+  mutate(presence = TRUE) |>
+  pivot_wider(
+    names_from = domain,
+    values_from = presence,
+    values_fill = FALSE,
+    names_sort = TRUE
+  ) |>
+  select(-any_of("NA"))
+
+
+absence_presence |>
+  left_join(ranks, join_by(genome, tax_id)) |>
+  relocate(genome, tax_id, species) |>
+  format_tsv() |>
+  writeLines(stdout(), sep = "")
