ec321ff2be4a08117bb211c28ab1586487048908
Emanuel Becerra Soto <elbecerrasoto@gmail.com>
2024-12-04 15:34:21 -0700

    pipe simplification with queries faa rule

--- workflow/scripts/harvest.R
+++ workflow/scripts/harvest.R
@@ -1,12 +1,15 @@
 #!/usr/bin/env Rscript
 
+# Works on anything
+# Does has the columts
+# genome & pid
+
 # Globals ----
 
 suppressPackageStartupMessages({
   library(tidyverse)
   library(furrr)
   library(seqinr)
-  library(fs)
 })
 
 argv <- commandArgs(trailingOnly = TRUE)
@@ -15,67 +18,69 @@ argv <- commandArgs(trailingOnly = TRUE)
 DB <- argv[[1]]
 CORES <- as.integer(argv[[2]])
 IN <- argv[[3]]
-OUT_DIR <- argv[[4]]
 
-# DB <- "tests/results/genomes"
-# IN <- "tests/results/hmmer.tsv"
-# OUT_DIR <- "tests/results/domains_faas"
+## DB <- "tests/results/genomes"
+## IN <- "tests/results/hmmer.tsv"
+## CORES <- 12
 
 plan(multicore, workers = CORES)
 
-# Helpers ----
+
+neis <- read_tsv(IN, show_col_types = FALSE)
+
+
+
+neis <- neis |>
+  distinct(pid, .keep_all = TRUE)
+
+Lgenomes <- neis %>%
+  split(., .$genome)
+
+
+write_genome <- function(genome_tib) {
+  genome <- unique(genome_tib$genome)
+  in_genome <- paste0(DB, "/", genome, "/", genome, ".faa")
+  pids <- unique(genome_tib$pid)
+
+  stopifnot(length(genome) == 1)
+
+  faa <- read.fasta(in_genome, seqtype = "AA", strip.desc = TRUE)
+  faa <- faa[names(faa) %in% pids]
+
+  cat(".", file = stderr())
+  flush.console()
+
+  faa
+}
+
+
+
+done <- future_map(Lgenomes, possibly(write_genome, NULL))
+
+out_len <- sum(map_int(done, length))
+out <- vector(mode = "list", length = out_len)
+
+i <- 1
+for (genome in done) {
+  for (faa in genome) {
+    out[[i]] <- faa
+    i <- i + 1
+  }
+}
 
 get_headers <- function(faa) {
   map_chr(faa, \(s) attr(s, "Annot"))
 }
 
-write_query <- function(query_tib) {
-  query_tib <- distinct(query_tib, pid, .keep_all = TRUE)
-  query <- unique(query_tib$query)
-  txt <- unique(query_tib$query_txt)
 
-  stopifnot(
-    length(query) == 1,
-    length(txt) == 1
+suppressWarnings({
+  # Suppresing Warning on file() call inside write.fasta
+  # Warning:
+  # In file(description = file.out, open = open) :
+  # using 'raw = TRUE' because '/dev/stdout' is a fifo or pipe
+  write.fasta(out, get_headers(out),
+    "/dev/stdout",
+    open = "a",
+    nbchar = 80
   )
-
-  OUT_FAA <- paste0(OUT_DIR, "/", query, "_", txt, ".faa")
-  unlink(OUT_FAA)
-
-
-  write_genome <- function(genome_tib) {
-    genome <- unique(genome_tib$genome)
-    in_genome <- paste0(DB, "/", genome, "/", genome, ".faa")
-    pids <- unique(genome_tib$pid)
-
-    stopifnot(length(genome) == 1)
-
-    faa <- read.fasta(in_genome, seqtype = "AA", strip.desc = TRUE)
-    faa <- faa[names(faa) %in% pids]
-
-    write.fasta(faa, get_headers(faa),
-      OUT_FAA,
-      open = "a",
-      nbchar = 80
-    )
-  }
-
-
-  Lgenomes <- query_tib %>%
-    split(., .$genome)
-
-  walk(Lgenomes, write_genome)
-
-  OUT_FAA
-}
-
-# Main ----
-
-hmmer <- read_tsv(IN, show_col_types = FALSE)
-
-Lqueries <- hmmer %>%
-  split(., .$query)
-
-dir_create(OUT_DIR)
-
-done <- future_map(Lqueries, write_query)
+})
