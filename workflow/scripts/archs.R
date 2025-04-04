585607e7654afa2cc5019a8095c1e5d43e64ca36
Emanuel Becerra Soto <elbecerrasoto@gmail.com>
2025-04-03 18:18:24 -0700

    broken, wip
    ---
    ranks X tax_id X genome X pid X queries X archs
    goal.tsv

--- workflow/scripts/archs.R
+++ workflow/scripts/archs.R
@@ -7,67 +7,15 @@ suppressPackageStartupMessages({
 
 argv <- commandArgs(trailingOnly = TRUE)
 
-ISCAN <- argv[[1]]
-OUT <- argv[[2]]
-OUT_PIDFOCUS <- argv[[3]]
-OUT_CODE <- argv[[4]]
+# ISCAN <- argv[[1]]
+# OUT <- argv[[2]]
+# OUT_PIDFOCUS <- argv[[3]]
+# OUT_CODE <- argv[[4]]
 
-# ISCAN <- "tests/results/iscan.tsv"
-# OUT <- "tests/results/archs.tsv"
-# OUT_PIDFOCUS <- "tests/results/archs_pidrow.tsv"
-# OUT_CODE <- "tests/results/archs_code.tsv"
-
-OFFSET <- 33
-PF_INT_LEN <- 5
-PF_LEAD_CHAR <- "PF"
-
-one_lettercode <- function(doms) {
-  # Avoids invisible characters
-
-  doms <- unique(doms)
-
-  pfam_chars <- str_extract(doms, "\\d+")
-  stopifnot("Bad PFAM ID." = all(str_length(pfam_chars) == PF_INT_LEN))
-
-  pfam_ints <- as.integer(pfam_chars)
-  stopifnot("Some extracted PFAM IDs are NA." = all(!is.na(pfam_ints)))
-
-  stopifnot("Unicode points out of range." = all((pfam_ints + OFFSET) <= 0x10FFFF))
-  pfam_codes <- strsplit(intToUtf8(pfam_ints + OFFSET), "")[[1]]
-
-  stopifnot("Conversion to utf-8 failed." = length(pfam_codes) == length(doms))
-  OUT <- as.list(pfam_codes)
-  names(OUT) <- doms
-
-
-  OUT
-}
-
-
-code_to_pfam <- function(codes) {
-  TOTAL_LEN <- PF_INT_LEN + str_length(PF_LEAD_CHAR)
-
-  pfam_ints <- utf8ToInt(codes) - OFFSET
-  pfam_chars <- as.character(pfam_ints)
-
-  appends <- map_chr(
-    PF_INT_LEN - str_length(pfam_chars),
-    \(x) ifelse(x > 0, str_flatten(rep("0", x)), "")
-  )
-
-  OUT <- str_c(PF_LEAD_CHAR, appends, pfam_chars)
-  stopifnot("Bad PFAM ID" = all(str_length(OUT) == TOTAL_LEN))
-
-  OUT
-}
-
-replace_to_oneletter <- function(archs, code) {
-  keys <- names(code)
-  for (key in keys) {
-    archs <- str_replace_all(archs, key, code[[key]])
-  }
-  str_replace_all(archs, ",", "")
-}
+ISCAN <- "tests/results/iscan.tsv"
+OUT <- "tests/results/archs.tsv"
+OUT_PIDFOCUS <- "tests/results/archs_pidrow.tsv"
+OUT_CODE <- "tests/results/archs_code.tsv"
 
 
 get_arch_len <- function(arch) {
@@ -79,56 +27,23 @@ get_arch_len <- function(arch) {
 
 iscan <- read_tsv(ISCAN, show_col_types = FALSE)
 
-archs <- iscan |>
-  filter(analysis == "Pfam") |>
-  group_by(pid) |>
-  reframe(
-    domain = memberDB, start = start, end = end,
-    length = length, domain_txt = memberDB_txt
-  ) |>
-  arrange(pid, start, end) |>
-  mutate(
-    start = as.integer(start),
-    end = as.integer(end),
-    length = as.integer(length)
-  )
+iscan_summary <- iscan |>
+  select(pid, memberDB, interpro, start, end, length, memberDB_txt) |>
+  arrange(pid, start)
 
-archs <- archs |>
-  group_by(pid) |>
-  reframe(order = 1:length(start), across(everything())) |>
-  relocate(order, .after = domain)
+any(str_detect(iscan_summary$memberDB, ","))
+any(str_detect(iscan_summary$memberDB, "\\|"))
 
+iscan_summary$memberDB[str_detect(iscan_summary$memberDB, "\\|")]
 
-pid_focus <- archs |>
+str_flatten(c("hi", "hello"), collapse = "|")
+
+stopifnot("Separator is used by a memberDB ID." = all(!str_detect(iscan_summary$memberDB, "\\|")))
+stopifnot("Unexpeted NA on memberDB field." = all(!is.na(iscan_summary$memberDB)))
+
+archs <- iscan_summary |>
   group_by(pid) |>
   summarize(
-    pid = unique(pid),
-    arch = str_flatten(domain, collapse = ",")
+    archMEM = str_flatten(memberDB, collapse = "|"),
+    archIPR = str_flatten(interpro[!is.na(interpro)], collapse = "|")
   )
-
-ONE_LETTER <- one_lettercode(archs$domain)
-
-pid_focus <- pid_focus |>
-  left_join(
-    distinct(archs, pid, length),
-    join_by(pid)
-  ) |>
-  mutate(ndoms = get_arch_len(arch)) |>
-  relocate(ndoms, .after = arch)
-
-
-pid_focus <- pid_focus |>
-  mutate(arch_code = replace_to_oneletter(arch, ONE_LETTER))
-
-one_letter_chr <- ONE_LETTER |>
-  unlist()
-
-one_letter_tib <- tibble(
-  domain = names(one_letter_chr),
-  letter = one_letter_chr
-)
-
-
-write_tsv(archs, OUT)
-write_tsv(pid_focus, OUT_PIDFOCUS)
-write_tsv(one_letter_tib, OUT_CODE)
